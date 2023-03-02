fdb876d22d035e0c8a0739acdb5571132dc22549
Parselinho <parselinho@gmail.com>
2023-03-02 12:32:23 +0200

    Update Board.js

--- js/Board.js
+++ js/Board.js
@@ -4,42 +4,36 @@ class Board {
         this.columns = 7;
         this.spaces = this.createSpaces();
     }
-
-//     createSpaces() {
-//         const space = [];
-//         for (let i = 0; i < this.rows; i++) { 
-//             const row = [];
-//             for (let j = 0; j < this.columns; i++) {
-//                 const space = new Space(i, j);
-//                 row.push(space);
-//             }
-//             this.spaces.push(row);
-//         }
-//         return spaces;
-//     }
-
-// }
-
+    
+    /** 
+     * Generates 2D array of spaces. 
+     * @return  {array}     An array of space objects
+     */
     createSpaces() {
         const spaces = [];
-
-        for (let x = 0; x < this.columns; x++) {
-            const col = [];
-
-            for (let y= 0; y < this.rows; y++) {
-                const space = new Space(x, y);
-                col.push(space);
-            }
-            spaces.push(col);
-        }
+        
+		for (let x = 0; x < this.columns; x++) {
+			const col = [];
+			
+			for (let y = 0; y < this.rows; y++) {
+				const space = new Space(x, y);
+				col.push(space);
+			}
+			
+			spaces.push(col);
+		}
+        
         return spaces;
     }
-
-    drawHTMLBoard() {
+    
+    /** 
+     * Draws associated SVG spaces for all game spaces.
+     */
+	drawHTMLBoard() {
         for (let column of this.spaces) {
             for (let space of column) {
                 space.drawSVGSpace();
             }
         }
-    }
+	} 
 }
